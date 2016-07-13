6673c627a0f9993f7d8b81bb5d1463e6445f8b51
Hoyub Lee <hoyub.lee@samsung.com>
2016-07-13 17:32:51 +0900

    Release tpl-test using googletest v0.2.0
    TPL Test module for testing libtpl-egl frontend APIs
    has been renewed with using Google C++ Test Framework.
    
    Build libtpl-egl:
        gbs build -A armv7l --spec=libtpl-egl.spec
    
    Build tpl-test:
        gbs build -A armv7l --spec=tpl-test.spec
    
    Build all:
        gbs build -A armv7l
    
    Change-Id: Ic076acc20eedd8fb9b95e3fe428cc28bdf41f78b
    Signed-off-by: Hoyub Lee <hoyub.lee@samsung.com>

---
+++ tc/src/main.cc
@@ -0,0 +1,23 @@
+/**
+ * @file main.cc
+ * @brief TPL Test main entry point
+ * 
+ * In main() function, test configurations must be set by user.
+ * Then, all tests will be run.
+ */
+
+#include "tpl-test.h"
+
+#include "gtest/gtest.h"
+
+
+int main(int argc, char **argv) {
+    // Setup configurations
+    TPLTest::width = 720;
+    TPLTest::height = 1280;
+    TPLTest::depth = 24;
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
---
+++ tc/src/tpl-test-object.cc
@@ -0,0 +1,48 @@
+/**
+ * @file tpl-test-object.cc
+ * @brief TPL Test case of object 
+ *
+ */ 
+
+#include "tpl-test.h"
+
+#include "gtest/gtest.h"
+
+
+TEST_F(TPLTest, tpl_object_get_type) {
+    int obj_type = -1;
+    obj_type = tpl_object_get_type((tpl_object_t *)tpl_display);
+    ASSERT_EQ(TPL_OBJECT_DISPLAY, obj_type);
+
+    obj_type = -1;
+    obj_type = tpl_object_get_type((tpl_object_t *)tpl_surface);
+    ASSERT_EQ(TPL_OBJECT_SURFACE, obj_type);
+}
+
+TEST_F(TPLTest, tpl_object_userdata_test) {
+    unsigned long key;
+
+    // set user data
+    tpl_object_set_user_data((tpl_object_t *)tpl_display, &key, (void *)wl_disp.display, NULL);
+
+    // get user data
+    void* get_dpy = NULL;
+    get_dpy = (void *)tpl_object_get_user_data((tpl_object_t *)tpl_display, &key);
+
+    ASSERT_NE((void *)NULL, get_dpy);
+}
+
+TEST_F(TPLTest, tpl_object_reference) {
+    // tpl_object_reference
+    tpl_object_reference((tpl_object_t *)tpl_display);
+
+    // tpl_object_get_reference
+    int ref_count = -1;
+    ref_count = tpl_object_get_reference((tpl_object_t *)tpl_display);
+    ASSERT_NE(-1, ref_count);
+
+    // tpl_object_unreference
+    int unref_count = -1;
+    unref_count = tpl_object_unreference((tpl_object_t *)tpl_display);
+    ASSERT_EQ(ref_count - 1, unref_count);
+}
